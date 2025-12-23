ff50193b1581995ef77f7a9190be9405633b3114
Lloyd Everett <lloydeverett@gmail.com>
2025-12-23 13:37:20 +0200

    add sandboxing example

--- example/main.lua
+++ example/main.lua
@@ -7,3 +7,7 @@ foo.bar()
 
 require("hello_world").hello_world_fn() -- exported from C
 
+local sandbox_env = { print = print }
+print("print function in sandbox = " .. tostring(load("return print", nil, 't', sandbox_env)()))
+print("require function in sandbox = " .. tostring(load("return require", nil, 't', sandbox_env)()))
+
